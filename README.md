b5458299244e5e24d884f1b26057b585f68e7bfc
phewson16 <peterhewson16@gmail.com>
2024-10-04 10:19:52 -0400

    second last commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# first-JS-script
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let message = "Hello";
 
 // if morning, show "Good Morning"
 if (current <= 12) {
-    message = "Good Morning, beatutiful word!";
+    message = "Good Morning, beautiful word!";
 } else if (currentHour > 12 && currentHour <= 17 ) {
     message = "Hello beautiful world";
 } else if (currentHour > 17) {
